3216f784ac5365e0c0d74848de5f30e1f6c1db91
Andrea Megna <andreamegna@Andrea-Megnas-MacBook-Pro.local>
2018-06-21 10:54:52 +0200

    env

--- index.js
+++ index.js
@@ -43,31 +43,34 @@ const getAnalytics = () => {
     .flatMap(() => Rx.Observable.bindNodeCallback(jsonfile.readFile)('./data/analytics.json', { encoding: 'utf8' }))
 }
 
+if (! fs.existsSync(process.env.FOLDER_DATA)) fs.mkdirSync(process.env.FOLDER_DATA);
+if (! fs.existsSync(process.env.FOLDER_CSV)) fs.mkdirSync(process.env.FOLDER_CSV);
+
 // GET SITEMAPS
 Rx.Observable.defer(() => sitemapper.fetch(process.env.SITEMAP_LINK))
 .map(res => res.sites.map(page => page.replace(/https?:[\/]{2}\S*?\/(\S*)/, '/$1')))
-.flatMap(res => Rx.Observable.bindNodeCallback(jsonfile.writeFile)('./data/sitemap.json', res, {spaces: 2}))
-.flatMap(() => Rx.Observable.bindNodeCallback(jsonfile.readFile)('./data/sitemap.json', { encoding: 'utf8' }))
+.flatMap(res => Rx.Observable.bindNodeCallback(jsonfile.writeFile)(process.env.FOLDER_DATA + '/sitemap.json', res, {spaces: 2}))
+.flatMap(() => Rx.Observable.bindNodeCallback(jsonfile.readFile)(process.env.FOLDER_DATA + '/sitemap.json', { encoding: 'utf8' }))
 // .do(console.log)
 .flatMap(sitemap => getAnalytics(), utilities.compareRowsToSitemap)
-.flatMap(res => Rx.Observable.bindNodeCallback(jsonfile.writeFile)('./data/result.json', res, {spaces: 2}))
-.flatMap(() => Rx.Observable.bindNodeCallback(jsonfile.readFile)('./data/result.json', { encoding: 'utf8' }))
+.flatMap(res => Rx.Observable.bindNodeCallback(jsonfile.writeFile)(process.env.FOLDER_DATA + '/result.json', res, {spaces: 2}))
+.flatMap(() => Rx.Observable.bindNodeCallback(jsonfile.readFile)(process.env.FOLDER_DATA + '/result.json', { encoding: 'utf8' }))
 .flatMap(input => {
 
     if (input.not_indexed.length) {
-        fs.writeFileSync('./data/csv/not_indexed.csv', json2csv(input.not_indexed.map(x => ({
+        fs.writeFileSync(process.env.FOLDER_CSV + '/not_indexed.csv', json2csv(input.not_indexed.map(x => ({
             not_indexed: x
         })), ['not_indexed']));
     }
     
     if (input.not_performed.length) {
-        fs.writeFileSync('./data/csv/not_performed.csv', json2csv(input.not_performed.map(x => ({
+        fs.writeFileSync(process.env.FOLDER_DATA + '/not_performed.csv', json2csv(input.not_performed.map(x => ({
             not_performed: x.page
         })), ['not_performed']));
     }
     
     if (input.best.length) {
-        fs.writeFileSync('./data/csv/best.csv', json2csv(input.best.map(x => ({
+        fs.writeFileSync(process.env.FOLDER_DATA + '/best.csv', json2csv(input.best.map(x => ({
             best: x.page
         })), ['best']));
     }
